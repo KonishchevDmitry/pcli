f80fb3f7d68ae757a367c48972fcd7743e6d7345
Dmitry Konishchev <konishchev@gmail.com>
2013-11-13 15:39:49 +0400

    Version 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     with open("README") as readme:
         setup(
             name = "pcli",
-            version = "0.1",
+            version = "0.2",
 
             description = readme.readline().strip(),
             long_description = readme.read().strip() or None,
